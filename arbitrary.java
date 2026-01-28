7e2ec4f2b2928c3a132db62d78220bb634e3bbd7
Jordyn Young <jordyn.young2234@gmail.com>
2026-01-28 11:32:47 -0500

    Create arbitrary.java

---
+++ arbitrary.java
@@ -0,0 +1,26 @@
+public class CoffeeCounter {
+
+    private int cupsDrunk;
+
+    public CoffeeCounter() {
+        this.cupsDrunk = 0;
+    }
+
+    public void drinkCup() {
+        cupsDrunk++;
+        System.out.println("You drank a cup of coffee ☕ Total: " + cupsDrunk);
+    }
+
+    public int getCupsDrunk() {
+        return cupsDrunk;
+    }
+
+    public static void main(String[] args) {
+        CoffeeCounter counter = new CoffeeCounter();
+
+        counter.drinkCup();
+        counter.drinkCup();
+
+        System.out.println("Final count: " + counter.getCupsDrunk());
+    }
+}
